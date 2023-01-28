f85a57b6e9db714dd7db19be39de4cc040618a9a
djalolovv <axellpubgm6@gmail.com>
2023-01-28 20:56:15 +0500

    tugatildi

--- main.js
+++ main.js
@@ -10,54 +10,23 @@ function fordate(userText) {
     .then((value) => value.json())
     .then((result) => data(result, userText));
 }
-
 function easy() {
   fordate();
 }
 
-function data(javob, soz) {
-  if (javob.title) {
-    content.innerHTML = `
-            <h2 class="topilmadi">Not found "${soz}"</h2>
-        `;
-  } else {
-    let text = javob[0].word,
-      fonetiki = javob[0].phonetic,
-      manosi = javob[0].meanings[0].definitions[0].definition,
-      masalan = javob[0].meanings[0].definitions[0].example,
-      foydalanilishi = javob[0].meanings[0].definitions[0].definition,
-      masalan2 = javob[0].meanings[0].definitions[0].example,
-      foydalanilishi2 = javob[0].meanings[0].definitions[0].definition,
-      audiosi = javob[0].phonetics[0].audio;
-
-    if (foydalanilishi == undefined) {
-      foydalanilishi = javob[0].meanings[1].definitions[0].definition;
-    } else {
-      foydalanilishi = "Not found using";
-    }
-
-    if (masalan == undefined) {
-      masalan = undefined;
-    }
-    if (fonetiki == undefined) {
-      phonetic = undefined;
-    }
-    if (masalan2 == undefined) {
-      masalan2 = undefined;
-    }
-    if (foydalanilishi2 == undefined || "") {
-      foydalanilishi2 = foydalanilishi2 =
-        javob[0].meanings[1].definitions[0].definition;
-    } else {
-      foydalanilishi = undefined;
-    }
-
-    if (audiosi == undefined) {
-      audiosi = javob[0].phonetics[1].audio;
-    }
-
-    content.innerHTML = "";
-    content.innerHTML = `
+function data(javob) {
+  let text = javob[0].word,
+    fonetiki = javob[0].phonetic,
+    manosi = javob[0].meanings[0].definitions[0].definition,
+    masalan = javob[0].meanings[0].definitions[0].example,
+    foydalanilishi = javob[0].meanings[0].definitions[0].definition,
+    masalan2 = javob[0].meanings[0].definitions[0].example,
+    foydalanilishi2 = javob[0].meanings[0].definitions[0].definition,
+    audiosi = javob[0].phonetics[0].audio;
+  javob[0].meanings[1].definitions[0].definition;
+  audiosi = javob[0].phonetics[1].audio;
+  content.innerHTML = "";
+  content.innerHTML = `
         <h3 class="form-title">
             ${text} - ${fonetiki}
         </h3>
@@ -78,6 +47,5 @@ function data(javob, soz) {
         </p>
         <audio controls autoplay src="${audiosi}" class="user-audiosi"></audio>
         `;
-    console.log(javob);
-  }
+  console.log(javob);
 }
